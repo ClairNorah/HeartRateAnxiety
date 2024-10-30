eeafdd5b43f50c837ccc3bc8f9bd8e930b7ac739
ClairNorah <43666455+ClairNorah@users.noreply.github.com>
2024-10-30 13:30:19 +0100

    added interaction logger

--- HeartRate WatchKit Extension/Views/HeartRateMesurementView.swift
+++ HeartRate WatchKit Extension/Views/HeartRateMesurementView.swift
@@ -21,6 +21,9 @@ struct HeartRateMeasurementView: View {
         .onAppear {
             InteractionLogger.logInteraction() // Log initial view appearance
         }
+        .onDisappear {
+            InteractionLogger.saveInteractionsToCSV() // Save interactions to CSV when view disappears
+        }
         .onReceive(heartRateMeasurementService.$currentHeartRate) { newHeartRate in
             print("Current Heart Rate: \(newHeartRate)") // For debugging
         }
